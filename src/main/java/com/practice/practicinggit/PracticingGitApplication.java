6ac48a8f5f6158280609aebfcd57a7b1965e185b
DRodriguez2 <diego.rodriguez@hcl.com>
2021-04-04 21:13:30 -0400

    first commit

---
+++ src/main/java/com/practice/practicinggit/PracticingGitApplication.java
@@ -0,0 +1,13 @@
+package com.practice.practicinggit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticingGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticingGitApplication.class, args);
+	}
+
+}
