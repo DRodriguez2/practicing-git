92373c720ba281b2e121879685165f4c999df05a
DRodriguez2 <diego.rodriguez@hcl.com>
2021-04-04 21:20:37 -0400

    added control

---
+++ src/main/java/com/practice/practicinggit/Controller.java
@@ -0,0 +1,11 @@
+package com.practice.practicinggit;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	public void someMethod() {
+		System.out.println("Original text");
+	}
+}
